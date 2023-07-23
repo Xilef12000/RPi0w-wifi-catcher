91d367708bf9e38f27048e7ccc02441de639ad6f
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-07-23 21:15:15 +0200

    fix setup #2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+cd /home/pi
+
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-rpi.gpio -y
-pip install sqlite-web
+sudo pip install sqlite-web
 
 git clone https://github.com/Xilef12000/RPi0w-wifi-catcher
 sudo chmod a+x RPi0w-wifi-catcher/run.sh
@@ -10,5 +12,6 @@ sudo cp RPi0w-wifi-catcher/wifi-catcher.service /etc/systemd/system/wifi-catcher
 sudo chmod 644 /etc/systemd/system/wifi-catcher.service
 
 touch /home/pi/RPi0w-wifi-catcher/wifi.db
+sudo python3 /home/pi/RPi0w-wifi-catcher/setup.py
 
 sudo systemctl enable wifi-catcher.service
