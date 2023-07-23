56d6955dac76e75037d1367ede788b935da6c664
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-07-23 18:11:33 +0200

    primitive setup = run

---
+++ run.sh
@@ -0,0 +1,2 @@
+python3 -m sqlite_web wifi.db --host=0.0.0.0
+sudo python3 wifi_catcher.py
---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-rpi.gpio -y
+pip install sqlite-web
