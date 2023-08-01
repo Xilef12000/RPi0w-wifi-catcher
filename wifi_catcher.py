21e5cbecc066b3b76d5a86e476180d58d3002de6
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-08-01 12:17:32 +0200

    new hat

--- wifi_catcher.py
+++ wifi_catcher.py
@@ -19,31 +19,52 @@ font = ImageFont.load_default()
 con = sqlite3.connect("/home/pi/RPi0w-wifi-catcher/wifi.db")
 cur = con.cursor()
 
-LED_PIN_R = 26
-LED_PIN_G = 21
-LED_PIN_B = 20
+LED_PIN_R = 13
+LED_PIN_G = 19
+LED_PIN_B = 26
 
-BUTTON_PIN = 4
+BUTTON_PIN = 21
 
 #GPIO.setwarnings(False)
 #GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED_PIN_R, GPIO.OUT)
 GPIO.setup(LED_PIN_G, GPIO.OUT)
 GPIO.setup(LED_PIN_B, GPIO.OUT)
 
-def text(txt = ""):
+
+txt_action = "test"
+txt_count = 0
+txt_count_now = 0
+def text():
 	draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
-	(font_width, font_height) = font.getsize(txt)
+	(font_width, font_height) = font.getsize("txt")
 	draw.text(
-	    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
-	    txt,
+	    (0, 0),
+	    str(txt_count),
+	    font=font,
+	    fill=255,
+	)
+	draw.text(
+	    (oled.width // 2, 0),
+	    str(txt_count_now),
+	    font=font,
+	    fill=255,
+	)
+	draw.text(
+	    (0, font_height),
+	    txt_action,
 	    font=font,
 	    fill=255,
 	)
 	oled.image(image)
 	oled.show()
 
+def action(txt = "", col = ""):
+	global txt_action
+	txt_action = txt
+	text()
+	color(col)
 def color(col = ""):
 	GPIO.output(LED_PIN_R, GPIO.LOW)
 	GPIO.output(LED_PIN_G, GPIO.LOW)
@@ -67,38 +88,37 @@ def color(col = ""):
 			
 color()
 while True:
-	text("scanning...")
-	color("G")
+	with con:
+		cur.execute("SELECT * FROM WIFI")
+	txt_count = len(cur.fetchall())
+	action("scanning...", "G")
 	content = iwlist.scan(interface="wlan0")
 	timeNow = time()
-	text("parsing...")
-	color("Y")
+	action("parsing...", "Y")
 	cells = iwlist.parse(content)
 	#print(datetime.utcfromtimestamp(timeNow).strftime('%Y-%m-%d %H:%M:%S'))
+	txt_count_now = len(cells)
 	for cell in cells:
 		#print(cell["mac"], cell["essid"], int(cell["channel"]), cell["encryption"])
 		with con:
 			con.execute("INSERT OR REPLACE INTO WIFI VALUES (?, ?, ?, ?, ?)", (cell["mac"], cell["essid"], int(cell["channel"]), cell["encryption"], timeNow))
-	text("waiting...")
-	color("B")
+	action("waiting...", "B")
 	br = False
 	timeEnd = time() + 5
 	while time() < timeEnd:
 		if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-			text("button...")
-			color("R")
+			action("button...", "R")
 			timeButtonEnd = time() + 3
 			while GPIO.input(BUTTON_PIN) == GPIO.LOW:
 				if time() > timeButtonEnd:
 					br = True
 					break
 			if not br:
-				text("waiting...")
-				color("B")
+				action("waiting...", "B")
 		if br:
 			break
 	if br:
 		break
-text("exiting...")
+setStatus("exiting...", "V")
 color("V")
 con.close()
